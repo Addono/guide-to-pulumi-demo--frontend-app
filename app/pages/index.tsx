4d9a66613a688f9e13429189e6b454ffffd4f586
Adriaan Knapen <hi@aknapen.nl>
2022-05-20 10:21:03 +0300

    feat: updates title to greet all Eficodians

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi there <a href="https://eficode.com">Eficodians</a>!
         </h1>
 
         <p className={styles.description}>
